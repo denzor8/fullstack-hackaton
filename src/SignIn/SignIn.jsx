c53f303cac0bd9462c7b5238e09925ceb9ef875a
Vasilev Vlad <vladvasiliev894@gmail.com>
2023-03-12 15:31:40 +0600

    updated theme

--- src/SignIn/SignIn.jsx
+++ src/SignIn/SignIn.jsx
@@ -1,101 +1,96 @@
 import React, { useState } from 'react';
 
-import { Button, Typography } from "@mui/material";
+import { Button, Typography, makeStyles } from "@mui/material";
 import TwitterIcon from '@mui/icons-material/Twitter';
 import SearchIcon from '@mui/icons-material/Search';
 // import { ModalBlock } from '../pages/components/ModalBlock';
 import TextField from '@mui/material/TextField';
 import PeopleIcon from '@mui/icons-material/People';
 import ModeCommentIcon from '@mui/icons-material/ModeComment';
-import { DialogActions ,DialogContent} from '@mui/material';
-
-
+import { DialogActions, DialogContent } from '@mui/material';
 import './SignIn.css'
 
+
 function SignIn() {
+  const [visibleModal, setVisibleModal] = useState('');
+  const handleClickOpenSignIn = () => {
+    setVisibleModal('signIn');
+  };
+  const handleClickOpenSignUp = () => {
+    setVisibleModal('signUp');
+  };
+  const handleCloseModal = () => {
+    setVisibleModal('');
+  };
 
-    const [visibleModal, setVisibleModal] = useState('');
-
-    const handleClickOpenSignIn = () => {
-        setVisibleModal('signIn');
-    };
-
-    const handleClickOpenSignUp = () => {
-        setVisibleModal('signUp');
-    };
-
-    const handleCloseModal = () => {
-        setVisibleModal('');
-    };
-
-    return (
-        <div className="wrapper">
-            <section className="aboutSide">
-                <div className="aboutSideWrapper">
-                    <ul>
-                        <li>
-                            <Typography
-                                variant="h5"
-                                className="aboutSideListItem">
-                                <SearchIcon
-                                    className="aboutSideListItemIcon"
-                                />
-                                Читайте о том, что вам интересно.
-                            </Typography>
-                        </li>
-                        <li>
-                            <Typography
-                                variant="h5"
-                                className="aboutSideListItem">
-                                <PeopleIcon
-                                    className="aboutSideListItemIcon"
-                                />
-                                Узнайте, о чем говорят в мире.
-                            </Typography>
-                        </li>
-                        <li>
-                            <Typography
-                                variant="h5"
-                                className="aboutSideListItem">
-                                <ModeCommentIcon
-                                    className="aboutSideListItemIcon"
-                                />
-                                Присоединяйтесь к общению.
-                            </Typography>
-                        </li>
-                    </ul>
-                </div>
-            </section>
-            <section className="loginSide">
-                <div className="loginSideWrapper">
-                    <TwitterIcon
-                        color="primary"
-                        className="loginSideIcon"
-                    />
-                    <Typography variant="h2" className="loginSideTitle">
-                        В курсе происходящего
-                    </Typography>
-                    <Typography>
-                        {' '}
-                        <b>Присоединяйтесь к Твиттеру прямо сейчас!</b>{' '}
-                    </Typography>{' '}
-                    <br />
-                    <Button
-                        onClick={handleClickOpenSignUp}
-                        style={{ marginBottom: 20 }}
-                        variant="contained"
-                        color="primary"
-                        fullWidth>
-                        Зарегистрироваться
-                    </Button>
-                    <Button
-                        onClick={handleClickOpenSignIn}
-                        variant="outlined"
-                        color="primary"
-                        fullWidth>
-                        Войти
-                    </Button>
-                    {/* <ModalBlock
+  return (
+    <div className="wrapper">
+      <section className="aboutSide">
+        <div className="aboutSideWrapper">
+          <ul>
+            <li>
+              <Typography
+                variant="h5"
+                className="aboutSideListItem">
+                <SearchIcon
+                  className="aboutSideListItemIcon"
+                />
+                Читайте о том, что вам интересно.
+              </Typography>
+            </li>
+            <li>
+              <Typography
+                variant="h5"
+                className="aboutSideListItem">
+                <PeopleIcon
+                  className="aboutSideListItemIcon"
+                />
+                Узнайте, о чем говорят в мире.
+              </Typography>
+            </li>
+            <li>
+              <Typography
+                variant="h5"
+                className="aboutSideListItem">
+                <ModeCommentIcon
+                  className="aboutSideListItemIcon"
+                />
+                Присоединяйтесь к общению.
+              </Typography>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section className="loginSide">
+        <div className="loginSideWrapper">
+          <TwitterIcon
+            color="primary"
+            className="loginSideIcon"
+          />
+          <Typography variant="h2" className="loginSideTitle">
+            В курсе происходящего
+          </Typography>
+          <Typography>
+            {' '}
+            <b>Присоединяйтесь к Твиттеру прямо сейчас!</b>{' '}
+          </Typography>{' '}
+          <br />
+          <Button
+            onClick={handleClickOpenSignUp}
+            style={{ marginBottom: 20 }}
+            variant="contained"
+            color="primary"
+            fullWidth>
+            Зарегистрироваться
+          </Button>
+          <Button
+            onClick={handleClickOpenSignIn}
+            variant="outlined"
+            color="primary"
+            fullWidth>
+            Войти
+          </Button>
+          {/* <ModalBlock
                         visible={visibleModal === 'signUp'}
                         onClose={handleCloseModal}
                         // classes={classes}
@@ -134,7 +129,7 @@ function SignIn() {
                             </Button>
                         </DialogActions>
                     </ModalBlock> */}
-                    {/* <ModalBlock
+          {/* <ModalBlock
                         visible={visibleModal === 'signIn'}
                         onClose={handleCloseModal}
                         // classes={classes}
@@ -166,10 +161,10 @@ function SignIn() {
                             </Button>
                         </DialogActions>
                     </ModalBlock> */}
-                </div>
-            </section>
         </div>
-    );
+      </section>
+    </div>
+  );
 }
 
 export default SignIn;
