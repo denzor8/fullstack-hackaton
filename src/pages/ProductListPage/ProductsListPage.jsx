4a86f736633d85bc269a9d9ad81ec23b12b825d2
User <user@Users-MacBook-Air.local>
2023-03-14 13:23:46 +0600

    add search

--- src/pages/ProductListPage/ProductsListPage.jsx
+++ src/pages/ProductListPage/ProductsListPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ProductList from '../../components/ProductList/ProductList'
 import Navbar from '../../components/Navbar/Navbar'
+import Search from '../../components/Search/Search'
 const ProductsListPage = () => {
   return (
-    <>
+    <div style={{display: 'flex', alignItems: 'center'}} >
       <Navbar />
       <ProductList />
-    </>
+      <Search/>
+    </div>
   )
 }
 
