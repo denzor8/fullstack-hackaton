9fe8cea2fca5d91ec27f69cd310d49a6edc82967
Vasilev Vlad <vladvasiliev894@gmail.com>
2023-03-14 10:30:12 +0600

    update main routes and profile page

---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Profile from '../../components/Profile/Profile'
+import Navbar from '../../components/Navbar/Navbar'
+const ProfilePage = () => {
+	return (
+		<>
+			<Navbar/>
+			<Profile />
+		</>
+	)
+}
+
+export default ProfilePage
