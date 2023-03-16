0c900b8ac89e7bf585438503806dd0b60534e343
User <user@Users-MacBook-Air.local>
2023-03-16 12:17:06 +0600

    add mess

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -34,16 +34,42 @@ const Messages = () => {
             </div>
            </div>
            
-           <div className="mess-chat">
-              <div>
-
-            <img style={{width: '55px', height: '55px', display: 'flex', justifyContent: 'center'}} src="../../../image/user_icon-1 png" alt="" />
-          
         </div>
-      </div>
+       
 
-      </div> 
+       <div className="mess_chat">
+          <div style={{width: '600px', margin: '0 auto', display: 'flex', justifyContent: 'center', textAlign: 'center', paddingTop: '2rem'}}>
+            <div>
+              <img style={{width: '59px', height: '59px'}} src="../../../image/user_icon-1.png" alt="" />
+              <h4 style={{color: 'white', fontSize: '15px', fontWeight: '700px'}} >Devon Lane</h4>
+              <span style={{paddingLeft: '5px', color: '#8899A6'}} >@johndue 23</span>
+              <h1 style={{fontSize: '18px', paddingTop: '1rem', width: '500px', color: 'white'}} >Segurança Pública para Virar o Jogo,A Defesa do Espaço Cívico</h1>
+              <h5 style={{paddingTop: '1rem', color: '#8899A6' }} >Joined December 2016 66.8K Followers</h5>
+              <h5 style={{color: '#8899A6' }} >Not followed by anyone you're following </h5>
+
+              <div style={{marginTop: '3rem'}} className='small-scretch-bottom'></div>
+
+              <div style={{display: 'flex', justifyContent: 'flex-end', paddingTop: '5rem'}} >
+                <div className='mess-go' >
+                  <div>
+                      <h1 className='go_title' >Hello</h1>
+                       <h5 className='go_link' >Delete</h5>
+                  </div>
+                </div>
+              </div>
+
+              <div className='mess_content'>
+                   <div style={{marginTop: '3rem'}} className='small-scretch-bottom'></div>
+                   <input type="search"  className='mess_cont_search' placeholder='Start a new message' />
+              </div>
+
+            </div>
+          </div>
+       </div>
+
+
     </div>
+
   )
 }
 
